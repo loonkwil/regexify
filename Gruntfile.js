cdc7745e49c36e208b5f2ca4cd550e66ec304fb3
Peter Simon <simonpepe@gmail.com>
2014-01-07 13:06:01 +0100

    Add grunt task for bumping the version number

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,18 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-bump');
 
   // Project configuration.
   grunt.initConfig({
     qunit: {
       files: ['test/index.html']
+    },
+    bump: {
+      options: {
+        files: ['package.json', 'bower.json'],
+        commitFiles: ['package.json', 'bower.json'],
+        push: false
+      }
     }
   });
 
