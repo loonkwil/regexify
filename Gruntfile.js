a6602bed8c3e0a8adcfaeaea6e10a695a04eb53a
Peter Simon <simonpepe@gmail.com>
2014-01-07 13:42:01 +0100

    Add grunt task for minifying CSS and JS
    You can execute with the `grunt dist` command.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,8 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-bump');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   // Project configuration.
   grunt.initConfig({
@@ -13,9 +15,39 @@ module.exports = function(grunt) {
         commitFiles: ['package.json', 'bower.json'],
         push: false
       }
+    },
+    uglify: {
+      dist: {
+        files: {
+          'js/packed/base.min.js': [
+            'bower_components/jquery/jquery.js',
+            'bower_components/bootstrap/js/button.js',
+            'bower_components/jquery.auto-grow/src/jquery.auto-grow.js',
+            'bower_components/regex-colorizer/regex-colorizer.js',
+            'bower_components/zeroclipboard/ZeroClipboard.js',
+            'js/escape.js',
+            'js/pattern.js',
+            'js/haystack.js',
+            'js/matches.js',
+            'js/regexify.js'
+          ]
+        }
+      }
+    },
+    cssmin: {
+      combine: {
+        files: {
+          'css/packed/main.min.css': [
+            'bower_components/bootstrap/dist/css/bootstrap.css',
+            'bower_components/jquery.auto-grow/src/auto-grow.css',
+            'css/style.css'
+          ]
+        }
+      }
     }
   });
 
   // Task to run tests
   grunt.registerTask('test', 'qunit');
+  grunt.registerTask('dist', ['uglify:dist', 'cssmin']);
 };
