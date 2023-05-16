24ca024c8f0f9262539632461ee83e9afe44bdcf
Peter Simon <1401202+loonkwil@users.noreply.github.com>
2023-05-16 22:20:52 +0200

    Replace node adapter with a static one

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import solid from "solid-start/vite";
+import staticAdapter from "solid-start-static";
 import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [
     solid({
-      ssr: false,
+      adapter: staticAdapter(),
     }),
   ],
   test: {
