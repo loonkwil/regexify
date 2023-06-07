fccc81f3b3e41973c3f32a564da15fccbaa370b7
Peter Simon <1401202+loonkwil@users.noreply.github.com>
2023-06-07 23:34:03 +0200

    Add GitHub action to deploy the page

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import solid from "solid-start/vite";
 import staticAdapter from "solid-start-static";
 import { defineConfig } from "vite";
 export default defineConfig({
+  base: "/regexify/",
   plugins: [
     solid({
       adapter: staticAdapter(),
